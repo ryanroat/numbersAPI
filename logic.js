23675e4e047df14ed0fcdd6d5e60a709658a37d2
Ryan Roat <ryanroat@gmail.com>
2020-02-15 11:30:31 -0500

    layout fixed by eslint

--- logic.js
+++ logic.js
@@ -1,33 +1,31 @@
+function rando(max) {
+    return Math.floor(Math.random() * Math.floor(max)) + 1; // returns a 'random' number btwn 1 and max inclusive
+}
 
-    function rando(max) {
-      return (Math.floor(Math.random() * Math.floor(max))+1);   // returns a 'random' number btwn 1 and max inclusive
-    }
+const randomButton = document.querySelector('#randBtn');
+randomButton.addEventListener('click', randomNumber);
 
-    let randomButton = document.querySelector("#randBtn");
-    randomButton.addEventListener('click',randomNumber);
+const numberInput = document.querySelector('#numberInput');
+numberInput.addEventListener('input', action);
 
-    let numberInput = document.querySelector("#numberInput");
-    numberInput.addEventListener('input',action);
+function randomNumber() {
+    const r = rando(100);
+    numberInput.value = r;
+    action();
+}
 
-    function randomNumber() {
-      let r = rando(100);
-      numberInput.value = r;
-      action();
-    }
-
-    function action(){
-      let number = numberInput.value;
-      let fact = document.querySelector('#fact');
-      console.log(number);
-      let xhr = new XMLHttpRequest();
-      xhr.open('GET', 'http://numbersapi.com/'+number);
-      xhr.onload = function (){
-        if (this.status == 200 && number !=""){
-          fact.innerHTML = this.responseText;
+function action() {
+    const number = numberInput.value;
+    const fact = document.querySelector('#fact');
+    // console.log(number);
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', `http://numbersapi.com/${number}`);
+    xhr.onload = function() {
+        if (this.status == 200 && number != '') {
+            fact.innerHTML = this.responseText;
+        } else {
+            fact.innerHTML = '';
         }
-        else {
-          fact.innerHTML = '';
-        }
-      }
-      xhr.send();
     };
+    xhr.send();
+}
